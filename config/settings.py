d18bb41a2cd87f187b0174d00fcf58c26cbe58c5
Winperingg <kleysoncruzeiro.soares490@gmail.com>
2026-03-10 00:21:06 -0300

    requirements e gunicorn

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zfy7y-h0treylqmu)_^3of)_5edd&lg#53qhyv9#(9wljz+@x7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
